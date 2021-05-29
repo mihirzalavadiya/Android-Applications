e5d022311219ef2e223decca74c041be4c71d533
mihirzalavadiya <mihirzalavadiya@gmail.com>
2021-05-29 17:06:10 +0530

    appbar

---
+++ AppBar/app/src/main/java/com/example/appbar/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.appbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar1);
+        toolbar.inflateMenu(R.menu.main_menu);
+    }
+
+    public boolean onCreateOptionsmenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+}

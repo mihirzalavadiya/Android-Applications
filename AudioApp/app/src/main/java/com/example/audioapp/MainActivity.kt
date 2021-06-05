856b16d0a10ae7c95bb78581dba7a6df04c28c62
mihirzalavadiya <mihirzalavadiya@gmail.com>
2021-06-05 15:22:16 +0530

    Audio App

---
+++ AudioApp/app/src/main/java/com/example/audioapp/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.example.audioapp
+
+import android.content.Context
+import android.media.AudioManager
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
+import android.widget.SeekBar
+import com.example.audioapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    var player: MediaPlayer? = null
+    private var oTime = 0
+    private var sTime: Int = 0
+    private var eTime: Int = 0
+    private var fTime: Int = 5000
+    private var bTime: Int = 5000
+    private val hdlr: Handler = Handler()
+    var audioManager: AudioManager? = null
+
+    fun play() {
+        player?.start()
+    }
+
+    fun pause() {
+        player?.pause()
+    }
+
+    fun stop() {
+        player?.stop()
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        player = MediaPlayer.create(this, R.raw.rabta)
+
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val seekVol = findViewById<SeekBar>(R.id.seekVol)
+
+        val maxVol = audioManager!!.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        seekVol.setMax(maxVol);
+
+        val curVol = audioManager!!.getStreamVolume(AudioManager.STREAM_MUSIC)
+        seekVol.setProgress(curVol)
+
+        seekVol.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                audioManager!!.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        // End Audio Manager
+
+        // Progress
+        val seekProg = findViewById<SeekBar>(R.id.seekProg)
+
+        player?.getDuration()?.let { duration -> seekProg.setMax(duration) }
+
+        seekProg.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                player!!.seekTo(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+        binding.play.setOnClickListener {
+            play()
+        }
+
+        binding.pause.setOnClickListener {
+            pause()
+        }
+
+        binding.stop.setOnClickListener {
+            stop()
+        }
+    }
+}
--- CardFlipAnimation/app/src/main/java/com/example/cardflipanimation/SharedAniationTwo.kt
+++ CardFlipAnimation/app/src/main/java/com/example/cardflipanimation/SharedAniationTwo.kt
@@ -2,11 +2,16 @@ package com.example.cardflipanimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cardflipanimation.databinding.ActivitySharedAniationTwoBinding
 
 class SharedAniationTwo : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySharedAniationTwoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shared_aniation_two)
+        binding = ActivitySharedAniationTwoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setTitle("Shared Animation Two")
     }

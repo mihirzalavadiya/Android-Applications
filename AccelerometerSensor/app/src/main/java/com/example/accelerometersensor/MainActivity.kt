30b089b80bf420a9721c9436c8ffcc5bb0a7c5f5
mihirzalavadiya <mihirzalavadiya@gmail.com>
2021-06-17 08:45:49 +0530

    Create Sensor App

---
+++ AccelerometerSensor/app/src/main/java/com/example/accelerometersensor/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.accelerometersensor
+
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var square: TextView
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        square = findViewById(R.id.tv_square)
+        accelerometerSensor()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    private fun accelerometerSensor() {
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_FASTEST, SensorManager.SENSOR_DELAY_FASTEST)
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            val sides = event.values[0]
+            val upDown = event.values[1]
+
+            square.apply {
+                rotationX = upDown * 3f
+                rotationY = sides * 3f
+                rotation = -sides
+                translationX = sides * -10
+                translationY = sides * 10
+            }
+
+            val squareColor = if (upDown.toInt() == 0 && sides.toInt() == 0) Color.GREEN else Color.rgb(233,30,99)
+            square.setBackgroundColor(squareColor)
+            square.text = "Up / Down ${upDown.toInt()}\nLeft / Right ${sides.toInt()}"
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
+    }
+}
